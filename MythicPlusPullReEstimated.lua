2daa0ef81e88840767a1e3c6b5b01840cb22fa23
Mark W <numedain@gmail.com>
2023-12-16 15:40:19 +0100

    minor cleanup

--- MythicPlusPullReEstimated.lua
+++ MythicPlusPullReEstimated.lua
@@ -828,7 +828,7 @@ function MMPE:InitPopup()
                 end
                 dialog.editBox:SetScript("OnEscapePressed", HidePopup);
                 dialog.editBox:SetScript("OnEnterPressed", HidePopup);
-                dialog.editBox:SetScript("OnKeyUp", function(_, key)
+                dialog.editBox:SetScript("OnKeyDown", function(_, key)
                     if IsControlKeyDown() and key == "C" then
                         HidePopup();
                     end
