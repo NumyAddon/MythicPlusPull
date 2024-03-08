e90ad2de7fc9918889a6e58b52dc02e80d78a705
Mark W <numedain@gmail.com>
2024-03-08 23:00:10 +0100

    Added localisation code, contribute on https://legacy.curseforge.com/wow/addons/mythicpluspullreestimated/localization

---
+++ .github/scripts/update_translation.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+# Thanks to Nevcairiel @ https://github.com/Nevcairiel/Bartender4/blob/master/locale/wowace-locale-import.sh
+
+cf_token=
+
+# Load secrets
+if [ -f ".env" ]; then
+	. ".env"
+fi
+
+[ -z "$cf_token" ] && cf_token=$CF_API_KEY
+
+declare -A locale_files=(
+  ["MythicPlusPullReEstimated"]="_MythicPlusPullReEstimated_locales.lua"
+)
+declare -A namespace_root=(
+  ["MythicPlusPullReEstimated"]="./"
+)
+
+tempfile=$( mktemp )
+trap 'rm -f $tempfile' EXIT
+
+do_import() {
+  namespace="$1"
+  file="$2"
+  : > "$tempfile"
+
+  echo -n "Importing $namespace..."
+  result=$( curl -sS -0 -X POST -w "%{http_code}" -o "$tempfile" \
+    -H "X-Api-Token: $CF_API_KEY" \
+    -F "metadata={ language: \"enUS\", namespace: \"$namespace\", \"missing-phrase-handling\": \"DeletePhrase\" }" \
+    -F "localizations=<$file" \
+    "https://legacy.curseforge.com/api/projects/431246/localization/import"
+  ) || exit 1
+  case $result in
+    200) echo "done." ;;
+    *)
+      echo "error! ($result)"
+      [ -s "$tempfile" ] && grep -q "errorMessage" "$tempfile" | jq --raw-output '.errorMessage' "$tempfile"
+      exit 1
+      ;;
+  esac
+}
+
+echo
+
+for namespace in "${!locale_files[@]}"; do
+  lua .github/scripts/find-locale-strings.lua "${namespace_root[$namespace]}" "${locale_files[$namespace]}"
+  do_import "$namespace" "${locale_files[$namespace]}"
+done
+
+exit 0
